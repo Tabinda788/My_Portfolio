b7915b18b58f5f67ce8ba53bc83dcd3896d56400
Tabinda 788 <tabindabhat21@gmail.com>
2021-11-13 16:31:27 +0530

    added staticfile

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -114,12 +114,12 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
+# STATIC_ROOT = [
+#     os.path.join(BASE_DIR, 'staticfiles')
+# ]
 django_heroku.settings(locals())
